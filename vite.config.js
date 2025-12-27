990963e10f3359ff89f6a073398fd5e7f5110ef7
anvithkashyap <anvithkashyap03@gmail.com>
2025-12-27 07:42:03 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/STAC-website/' : '/',
   server: {
     port: 3000,
     open: true
+  },
+  preview: {
+    port: 3000
   },
   build: {
     outDir: 'dist',
